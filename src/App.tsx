541414e2a2a65c468b36767a86ad5f4663c27720
Dang-Dat <112304912+Dang-Dat@users.noreply.github.com>
2026-04-05 14:55:44 +0700

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import { Toaster } from "sonner";
+
+function App() {
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+
+
+          <Route path="/" element={<ChatAppPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
